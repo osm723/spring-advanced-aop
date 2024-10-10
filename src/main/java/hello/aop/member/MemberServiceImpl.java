03fbb4180952f0202ab37c0b3edbfd1fb45d5841
Oh <osm723@gmail.com>
2024-10-10 17:22:54 +0900

    스프링 핵심 원리 - 고급편 섹션 13. 다음으로 다음으로

---
+++ src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.aop.member;
+
+import hello.aop.member.annotation.ClassAop;
+import org.springframework.stereotype.Component;
+
+@ClassAop
+@Component
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public String hello(String param) {
+        return "ok";
+    }
+
+    public String internal(String param) {
+        return "ok";
+    }
+}
---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,36 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // jdk 동적 프록시
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        Assertions.assertThrows(ClassCastException.class, () ->
+        {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // jdk 동적 프록시
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+
+    }
+}
